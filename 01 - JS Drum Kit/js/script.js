aa0369f92096d72b0aa1c44288d4cecfa976e8f0
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-03-28 23:20:42 +0000

    starting the exercise 01 oficially

--- 01 - JS Drum Kit/js/script.js
+++ 01 - JS Drum Kit/js/script.js
@@ -1,1 +1,9 @@
 // JAVASCRIPT 
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return; //if it's null, ends the functions
+
+    audio.currentTime = 0; //used to refresh the sound in case of multiple key hits
+    audio.play(); //check audio dom for more information about audio
+} );
