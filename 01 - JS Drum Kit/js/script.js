a8ee691d4d6b8be41297fe8af708a7873d05b090
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-03-29 00:21:15 +0000

    exe 01 finished

--- 01 - JS Drum Kit/js/script.js
+++ 01 - JS Drum Kit/js/script.js
@@ -1,9 +1,26 @@
-// JAVASCRIPT 
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     
     if(!audio) return; //if it's null, ends the functions
 
     audio.currentTime = 0; //used to refresh the sound in case of multiple key hits
     audio.play(); //check audio dom for more information about audio
-} );
+
+    key.classList.add('playing');
+
+}
+
+function removeTransition(e) {
+     //we just want the transform property and then after transform happens, we remove the class
+    if (e.propertyName !== "transform")  return; 
+
+    this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
