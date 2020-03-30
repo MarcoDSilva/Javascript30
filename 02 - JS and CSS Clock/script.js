dd1c8d734fb0a610d8ec686fb762aba9fa2c7207
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-03-30 19:29:49 +0100

    finished exercise.

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,1 +1,43 @@
-// alert("working");
+//variables for the digital clock
+const digitalHour = document.querySelector('.hours');
+const digitalMinute = document.querySelector('.minutes');
+const digitalSecond = document.querySelector('.seconds');
+
+//variables for analog clock
+const secondsHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    //updating digital clock
+    digitalSecond.textContent = seconds;
+    digitalMinute.textContent = minutes;
+    digitalHour.textContent = hours;
+
+    //updating analog clock
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const hoursDegrees = (hours / 12) * 360 + 90;
+
+    if(seconds === 59) {
+        secondsHand.classList.remove('.transition');
+        
+    } else if(seconds >= 1 && seconds <= 58)  {
+        secondsHand.classList.add('.transition');
+    }
+
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+}
+
+
+setInterval(setDate, 1000); //updates/calls setInterval each second
+
+
